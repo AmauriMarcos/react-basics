1bfedf8e1868e30f7be61a1070c7fa23e37cff24
AmauriMarcos <amaurisantos.m@gmail.com>
2021-02-18 16:21:21 +0100

    first commit

---
+++ src/Person/Person.js
@@ -0,0 +1,11 @@
+import '../Person/Person.css'
+
+const person = () => {
+    return(
+        <div className="container">
+            <p>O sapo gosta de pular.</p>
+        </div>
+    )
+}
+
+export default person;
